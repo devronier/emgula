2a15ba902948c48f1f710d8ff6285292708b9bf6
Ronier Lima <ronier.lim@gmail.com>
2024-10-21 21:44:48 -0300

    fim da segunda metade da primeira parte

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Cardapio from "../pages/Cardapio";
 import Contato from "../pages/Contato";
 import Sobre from "../pages/Sobre";
 
 function Main(props) {
-  let titulo = "";
-
-  switch (location.pathname) {
-    case "/sobre":
-      titulo = <Sobre />;
-      break;
-
-    case "/cardapio":
-      titulo = <Cardapio />;
-      break;
-
-    case "/contato":
-      titulo = <Contato />;
-      break;
-  }
-
   return (
     <main className={`p-6 ${props.dark ? "bg-gray-900" : "bg-gray-100"}`}>
-      {titulo}
+      <Routes>
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/cardapio" element={<Cardapio />} />
+        <Route path="/contato" element={<Contato />} />
+
+        <Route path="*" element={<h1>404 - Página não encontrada</h1>}></Route>
+      </Routes>
     </main>
   );
 }
